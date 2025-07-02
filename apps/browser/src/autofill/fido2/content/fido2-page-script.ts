bbb3d54045d229cf901805c6b2f7ebfcaf0deeed
Myrrh Periwinkle <myrrhperiwinkle@qtmlabs.xyz>
2025-07-02 09:00:31 +0700

    WebAuthn: ignore creation with attachment=cross-platform

--- apps/browser/src/autofill/fido2/content/fido2-page-script.ts
+++ apps/browser/src/autofill/fido2/content/fido2-page-script.ts
@@ -35,7 +35,6 @@ import { Messenger } from "./messaging/messenger";
   const BrowserAuthenticatorAttestationResponse = globalContext.AuthenticatorAttestationResponse;
 
   const browserNativeWebauthnSupport = globalContext.PublicKeyCredential != undefined;
-  let browserNativeWebauthnPlatformAuthenticatorSupport = false;
   if (!browserNativeWebauthnSupport) {
     // Polyfill webauthn support
     try {
@@ -59,17 +58,9 @@ import { Messenger } from "./messaging/messenger";
       /* empty */
     }
   } else {
-    void BrowserPublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable().then(
-      (available) => {
-        browserNativeWebauthnPlatformAuthenticatorSupport = available;
-
-        if (!available) {
-          // Polyfill platform authenticator support
-          globalContext.PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable = () =>
-            Promise.resolve(true);
-        }
-      },
-    );
+    // Polyfill platform authenticator support
+    globalContext.PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable = () =>
+      Promise.resolve(true);
   }
 
   const browserCredentials = {
@@ -99,12 +90,14 @@ import { Messenger } from "./messaging/messenger";
       return await browserCredentials.create(options);
     }
 
-    const authenticatorAttachmentIsPlatform =
-      options?.publicKey?.authenticatorSelection?.authenticatorAttachment === "platform";
+    const authenticatorAttachmentIsCrossPlatform =
+      options?.publicKey?.authenticatorSelection?.authenticatorAttachment === "cross-platform";
+    const fallbackSupported = browserNativeWebauthnSupport;
 
-    const fallbackSupported =
-      (authenticatorAttachmentIsPlatform && browserNativeWebauthnPlatformAuthenticatorSupport) ||
-      (!authenticatorAttachmentIsPlatform && browserNativeWebauthnSupport);
+    if (authenticatorAttachmentIsCrossPlatform && fallbackSupported) {
+      return await browserCredentials.create(options);
+    }
+
     try {
       const response = await messenger.request(
         {
